81e374c3b7fa9298defc13cedde0a22f0e030f63
Vinchurkar <siddhantvinchurkar@gmail.com>
2018-03-23 14:45:34 +0530

    Improved PWA scores

---
+++ sw.js
@@ -0,0 +1,27 @@
+importScripts('cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+	e.waitUntil(
+		caches.open('liveboard').then(function(cache) {
+			return cache.addAll([
+				'index.html',
+				'broadcast/index.html',
+				'watch/index.html',
+				'images/logo.png',
+				'images/logo192.png',
+				'css/materialize.min.css',
+				'manifest.json',
+				'js/materialize.min.js'
+			]);
+		})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	console.log(event.request.url);
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
